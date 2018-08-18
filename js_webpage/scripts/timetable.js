ddd896b689eef70c55f486ded85451e02978f2f8
hilmarm <hilmarmag@gmail.com>
2018-08-18 15:42:12 +0200

    cleaned up most files

--- js_webpage/scripts/timetable.js
+++
@@ -1,6 +0,0 @@
-var timetable = new Timetable();
-timetable.setScope(9, 17); // optional, only whole hours between 0 and 23
-timetable.addLocations(['Silent Disco', 'Nile', 'Len Room', 'Maas Room']);
-timetable.addEvent('Frankadelic', 'Nile', new Date(2015,7,17,10,45), new Date(2015,7,17,12,30));
-var renderer = new Timetable.Renderer(timetable);
-renderer.draw('.timetable'); // any css selector
